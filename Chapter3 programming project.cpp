d45011502ab5648d0850901022e39c965207fafc
kylebell7799@gmail.com <kylebell7799@gmail.com>
2024-10-02 00:27:04 -0400

    finished up the project

--- Chapter3 programming project.cpp
+++ Chapter3 programming project.cpp
@@ -1,20 +1,60 @@
 // Chapter3 programming project.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
+// Kyle Bell
+// CSC221
+
 #include <iostream>
+#include <iomanip>
+
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double sugar, butter, flour, indS, indB, indF, cookies;
+
+
+
+   
+
+    indS = 1.50 / 48;
+
+    indB = 1.00 / 48;
+
+    indF = 2.75 / 48; /* establishing the individual value for a single cookie */
+
+    cout << "Hello! How many cookies would you like to make today? " << endl;
+
+    cout << "\n"; // adding space between lines for a little tidyness
+
+    cin >> cookies;  /* user inout, going to be multiplied by the value for each 
+    ingredient to get the amount for any given amount of cookies*/
+
+    cout << "\n";
+  
+    sugar = indS * cookies;
+
+    butter = indB * cookies;
+
+    flour = indF * cookies; // formulas to get the adjusted ingredients list 
+    
+    cout << "The recipe requires " << endl;
+
+    cout << "\n";
+            
+    cout << fixed << setprecision(2) << sugar << " cups of sugar" << endl;
+
+    cout << "\n";
+
+    cout << fixed << setprecision(2) << butter << " cups of butter" << endl;
+
+    cout << "\n";
+
+    cout << fixed << setprecision(2) << flour << " cups of flour" << endl; /* setprecision and fixed
+    manipulators were used to give results rounded to two decimal places */
+
+    return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
