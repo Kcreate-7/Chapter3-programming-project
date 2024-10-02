411c350a26142ba85cf04160ef0320dc4bbd0fed
kylebell7799@gmail.com <kylebell7799@gmail.com>
2024-10-02 12:28:46 -0400

    just need to add comments

--- Chapter3 programming project.cpp
+++ Chapter3 programming project.cpp
@@ -2,19 +2,72 @@
 //
 
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int main()
+
+
 {
-    std::cout << "Hello World!\n";
+    float interestRate, principle, interestAmnt, savingsAmount, interest;
+    int timesCompounded;
+
+    cout << "Hello! What is your principle investment? " << endl;
+
+    cin >> principle;
+
+    cout << "\n";
+
+    cout << "Okay, now what is your interest rate? " << endl;
+
+    cin >> interestRate;
+
+    cout << "\n";
+
+    cout << "How many times has the interest compounded? " << endl;
+
+    cout << "(if it is compouned quarterly, it would be 4 times)" << endl;
+
+    cout << "\n";
+
+    cin >> timesCompounded;
+
+    cout << "\n";
+
+    cout << "\n";
+
+    interest = interestRate / 100;
+
+    interestAmnt = principle * (interestRate / 100);
+
+    savingsAmount = principle * pow(1 + (interest / timesCompounded), timesCompounded);
+
+    cout << setprecision(2) << fixed << showpoint;
+
+    cout << "Your interest rate is: " << setw(12) << interestRate << "% " << endl;
+
+    cout << "\n";
+
+    cout << "Times compounded:" << setw(19) << timesCompounded << endl;
+
+    cout << "\n";
+
+    cout << "Principle amount: " << setw(11) << "$" << principle << endl;
+    
+    cout << "\n";
+
+    cout << "Total interest gained: " << setw(8) << "$" << savingsAmount - principle << endl;
+
+    cout << "\n";
+
+    cout << "Total amount in savings: " << setw(4) << "$" << savingsAmount << endl;
+
+    return 0;
+
+
+
+
+    
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
